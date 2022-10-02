049749b25619811e1710fd70edae2c0e8093ad30
Alexander Bartolo <abartolo2@gmail.com>
2022-10-02 12:46:57 -0400

    add initial extended theme and structure of site

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,10 +4,18 @@ import { PageWrapper } from "../components/PageWrapper";
 const Home: NextPage = () => {
   return (
     <PageWrapper title="Home | Alexander Bartolo">
-      <div className="flex w-full bg-blue-500 p-4">
-        <h1>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+      <div className="flex w-full bg-gray p-4 min-h-[500px]">
+        <div className="w-full text-center">
+          <p className="text-white">Photo</p>
+          <p className="text-white">Description about myself</p>
+          <p className="text-white">Resume CTA</p>
+        </div>
+      </div>
+
+      <div className="flex w-full bg-blue p-4 min-h-[500px]">
+        <div className="w-full text-center">
+          <p className="text-white">Work Experience</p>
+        </div>
       </div>
     </PageWrapper>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blue: "#3B4358",
+        orange: "#D0826F",
+        blue: "#3B4358",
+        "blue-dark": "#223547",
+        gray: "#DFE0E0",
+      },
+    },
   },
   plugins: [],
 };
